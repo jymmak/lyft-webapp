dccd88025c7746c222d4706d0f71e30cadd40802
jymmak <henryfrancisco_fernando@hotmail.com>
2017-12-31 18:21:00 -0500

    funcionalidad boton2 de segunda vista

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@ $(document).ready(function () {
   /**Splash*/
   $('.title').delay(2000).fadeOut("slow");
 });
+
+ /**Activación de boton2 signup*/
+$("#btn2").click(function(){
+  $(".view1").hide();
+});
