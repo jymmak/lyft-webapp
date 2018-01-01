7298a7698d9bee7db2a74819f03f5562c28fa517
jymmak <henryfrancisco_fernando@hotmail.com>
2017-12-31 20:57:28 -0500

    second view

--- js/app.js
+++ js/app.js
@@ -4,7 +4,36 @@ $(document).ready(function () {
   $('.title').delay(2000).fadeOut("slow");
 });
 
- /**Activación de boton2 signup*/
-$("#btn2").click(function(){
+/**Activación de boton2 signup*/
+$("#btn2").click(function () {
+  $(".view2").show();
   $(".view1").hide();
 });
+
+/**Segunda vista*/
+/**Función del icono atrás*/
+$(".backico").click(function () {
+  $(".view1").show();
+  $(".view2").hide();
+});
+/**Función de btn phonee para que acepte 9dígitos y sólo números*/
+$('#phonee').keyup(function () {
+  if ($(this).val().length > 9) {
+    $('#submitBtn').removeAttr('disabled');
+  } else {
+    $('#submitBtn').attr('disabled', 'disabled');
+  }
+}).trigger('keyup');
+$(function () {
+  $('.phonee').keyup(function (e) {
+    if (this.value != '-')
+      while (isNaN(this.value))
+        this.value = this.value.split('').reverse().join('').replace(/[\D]/i, '')
+          .split('').reverse().join('');
+  })
+    .on("cut copy paste", function (e) {
+      e.preventDefault();
+    });
+});
+
+/**Tercera vista*/
